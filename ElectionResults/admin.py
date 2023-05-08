b017cd9de4ea813c082b49f86e8987537f22abbe
Tolu Joel <toluisjoel@gmail.com>
2023-05-08 18:56:17 +0100

    Customised admin page

--- ElectionResults/admin.py
+++ ElectionResults/admin.py
@@ -2,18 +2,54 @@ from django.contrib import admin
 
 from .models import *
 
-app_models = [
-    State,
-    Ward,
-    Lga,
-    PollingUnit,
-    Party,
-    Agentname,
-    AnnouncedLgaResult,
-    AnnouncedPuResult,
-    AnnouncedStateResult,
-    AnnouncedWardResult,
-]
+# Inlines
+class WardInline(admin.TabularInline):
+    model = Ward
+    extra = 0
 
-for model in app_models:    
-    admin.site.register(model)
+
+class PollingUnitInline(admin.TabularInline):
+    model = PollingUnit
+    extra = 0
+
+
+class AnnouncedPollingUnitResultInline(admin.TabularInline):
+    model = AnnouncedPollingUnitResult
+    extra = 0
+
+
+# Model Admins
+class WardAdmin(admin.ModelAdmin):
+    list_display = ['name', 'lga']
+    
+
+class MainWardAdmin(admin.ModelAdmin):
+    inlines = [WardInline]
+
+
+class PollingUnitAdmin(admin.ModelAdmin):
+    list_display = ['name', 'number', 'ward', 'lga']
+    list_filter = ['lga']
+    inlines = [AnnouncedPollingUnitResultInline]
+
+
+class LGAAdmin(admin.ModelAdmin):
+    list_display = ['name', 'state']
+    inlines = [PollingUnitInline]
+
+
+class MainPollingUnitAdmin(admin.ModelAdmin):
+    inlines = [PollingUnitInline]
+
+
+class AnnouncedPollingUnitResultAdmin(admin.ModelAdmin):
+    list_display = ['polling_unit', 'party', 'party_score']
+    list_filter = ['party']
+
+
+admin.site.register(State)
+admin.site.register(Party)
+admin.site.register(Lga, LGAAdmin)
+admin.site.register(Ward, WardAdmin)
+admin.site.register(PollingUnit, PollingUnitAdmin)
+admin.site.register(AnnouncedPollingUnitResult, AnnouncedPollingUnitResultAdmin)
